a8de4408366a5f9cf776f3e7a57df315ac1eafe5
koles1ko <koles1ko@yandex.ru>
2019-11-05 13:56:26 +0300

    start project

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+
+  // $('.menu__btn').on('click', function(){
+  //   $('.menu__list').slideToggle();
+  // }); Для бургера!
+
+  //   $('.class').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 4,
+  // }); Обычный одиночный слайдер!
+
+	//  $('.slider-for').slick({
+	//   slidesToShow: 1,
+	//   slidesToScroll: 1,
+	//   arrows: false,
+	//   fade: true,
+	//   asNavFor: '.slider-nav'
+	// });
+	// $('.slider-nav').slick({
+	//   slidesToShow: 3,
+	//   slidesToScroll: 1,
+	//   asNavFor: '.slider-for',
+	//   dots: true,
+	//   centerMode: true,
+	//   focusOnSelect: true
+	// }); Двойной слайдер!
+
+
+  // $('.wrapper .tab').on('click', function(event) {
+  // var id = $(this).attr('data-id');
+  //   $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+  //   $('.wrapper .tabs').find('.tab').removeClass('active');
+  //   $(this).addClass('active');
+  //   $('#'+id).addClass('active-tab').fadeIn();
+  //   return false;
+  // }); Для табов!
+
+});
